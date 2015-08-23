4778a385cbfd3b25f7c2f79dc2130aa6f7bc41d0
goliney <sergey@goliney.com>
2015-08-23 16:15:44 +0300

    Initial cards-slider commit

---
+++ src/common/cards-slider/cards-slider.constants.js
@@ -0,0 +1,10 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('common.cards-slider')
+    .constant('API', {
+      card: 'http://ec2-52-25-151-129.us-west-2.compute.amazonaws.com:8888/card/'
+    });
+
+})();
