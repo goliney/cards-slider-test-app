076fea2d858e4dfd7968fbac7b21d861215df0de
goliney <sergey@goliney.com>
2015-08-23 16:33:02 +0300

    Use mock data, as demo server doesn't support CORS

--- build/src/common/cards-slider/cards-slider.constants.js
+++ build/src/common/cards-slider/cards-slider.constants.js
@@ -4,7 +4,12 @@
   angular
     .module('common.cards-slider')
     .constant('API', {
-      card: 'http://ec2-52-25-151-129.us-west-2.compute.amazonaws.com:8888/card/'
+      root: 'http://ec2-52-25-151-129.us-west-2.compute.amazonaws.com:8888/',
+      /*
+      Unfortunately, demo server doesn't support CORS, so I use mock data instead
+       */
+      //card: 'http://ec2-52-25-151-129.us-west-2.compute.amazonaws.com:8888/card/'
+      card: 'assets/mock/card/'
     });
 
 })();
